fb0ce3242a2fded44573cc494130af54b9d594e7
Talha60970 <talhasaiyed007@gmail.com>
2026-08-19 12:41:38 +0530

    Add files via upload

---
+++ TALHA2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ float a,b;
+ printf("enter the amount in rupees");
+ scanf("%f",&a);
+ b=a/48;
+ printf("amount in $$ : %f",b);
+ getch();
+}
