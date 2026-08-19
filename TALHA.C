3ada9500b59518e371947e2079a2a0529399e670
Talha60970 <talhasaiyed007@gmail.com>
2026-08-19 12:23:28 +0530

    Add files via upload

---
+++ TALHA.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+ void main()
+{
+ clrscr();
+ float  a,b,c,d,e,f;
+ printf("enter the two nos");
+ scanf("%f%f",&a,&b);
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=1.0*a/b;
+  printf("aswers: %f %f %f %f",c,d,e,f);
+  getch();}
